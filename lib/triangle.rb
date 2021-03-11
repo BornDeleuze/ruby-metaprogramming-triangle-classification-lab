85c816b9f2d0175a311e21c0da05f9b889692ae3
Matt Beaudoin <ehxtwo@gmail.com>
2021-03-11 12:08:40 -0800

    commit tree 8a59990519c49c0cad06bc6d6c5ed2e045f436e9

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+
+  attr_reader :length3, :length2, :length1,:kind
+
+  def initialize(length1,length2,length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+    
+    if length1 == nil
+      raise TriangleError
+    end
+  end
+
+  def kind
+    if length1 <= 0 || length2 <= 0 || length3 <= 0 || length1 + length2 <= length3 || length2 + length3 <= length1 || length1 + length3 <= length2
+       raise TriangleError
+
+    elsif length1==length2 && length1== length3
+      kind = :equilateral
+
+    elsif length1== length2 || length1 == length3 || length2 == length3
+      kind = :isosceles
+
+    else
+      kind = :scalene
+    end
+
+    kind
+  end
+
+  class TriangleError < StandardError
+    #triangle error code
+
+  end
+
+
 end
